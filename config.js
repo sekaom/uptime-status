05ea5bb678330f380c265716c097bdcbcb19bced
sekaom <dulin081682@126.com>
2023-06-09 23:12:25 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Craft233 Status?',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2168378-7b9d6d0bc20d6bff4a8586b7'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Craft233官网',
+      url: 'https://craft233.top/'
+    },
+  ],
+};
